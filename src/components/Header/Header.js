afed42ed6c29245ef62967e8db6ce89614f544b9
Sabik52 <rahman52neajur@gmail.com>
2022-04-16 01:35:10 +0600

    component added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+      <div>
+          This is home
+      </div>
+    );
+};
+
+export default Header;
