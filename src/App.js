ab52f2a1bb8549a2904c8173ae0aa59042dc14b1
Sabik52 <rahman52neajur@gmail.com>
2022-04-16 13:10:18 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import About from './components/About/About';
 import Blogs from './components/Blogs/Blogs';
 import Deshboard from './components/Deshboard/Deshboard';
+import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Notfound from './components/Notfound/Notfound';
 import Review from './components/Reviews/Review';
@@ -11,6 +12,7 @@ import Review from './components/Reviews/Review';
 function App() {
   return (
     <div className="App">
+      <Header></Header>
      <Routes>
        <Route path='/' element={<Home></Home>}></Route>
        <Route path='/home' element={<Home></Home>}></Route>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import CustomLink from '../CustomLink/CoustomLink';
+import './Header.css'
+
 
 
 const Header = () => {
     return (
-      <div>
-       
-      </div>
+      
+       <nav>
+           <CustomLink to='/home'>Home</CustomLink>
+           <CustomLink to='/review'>Review</CustomLink>
+           <CustomLink to='/deshboard'>Deshboard</CustomLink>
+           <CustomLink to='/blogs'>Blogs</CustomLink>
+           <CustomLink to='/about'>About</CustomLink>
+       </nav>
+     
     );
 };
 
