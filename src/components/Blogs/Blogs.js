1e46ac189bbf1edf4a265d636690a92af98abcec
Sabik52 <rahman52neajur@gmail.com>
2022-04-18 01:23:32 +0600

    blogs added

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -3,7 +3,12 @@ import React from 'react';
 const Blogs = () => {
     return (
         <div>
-            <h2>This is blog</h2>
+            <h4>Q.No:1 What is Context API</h4>
+            <p>Answer: Context API is Share state within a component tree quaite easily.In React apps we normally use props to share the data to the child components, but in some cases, we need to pass the same prop to many components in such cases context API shines  </p>
+
+            <h4>Q.No:2 What is semantic Tag..?</h4>
+            <p>Answer: A semantic element clearly describes its meaning to both the browser and the developer. Examples of semantic elements: form , table , and article- Clearly defines its content. </p>
+
         </div>
     );
 };
